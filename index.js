1ce06db8324c0230bf13efc17ebd180346415548
Brandon Collins <brandontcollins08@gmail.com>
2025-03-11 15:28:41 -0500

    updating jscript

--- index.js
+++ index.js
@@ -1,34 +1,28 @@
 const initialCards = [
   {
     name: "Val Thorens",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/1-photo-by-moritz-feldmann-from-pexels.jpg",
   },
   {
     name: "Restaurant terrace",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/2-photo-by-ceiline-from-pexels.jpg",
   },
   {
     name: "An outdoor cafe",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/3-photo-by-tubanur-dogan-from-pexels.jpg",
   },
 
   {
     name: "A very long bridge, over the forest and through the trees",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/4-photo-by-maurice-laschet-from-pexels.jpg",
   },
   {
     name: "Tunnel with morning light",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/5-photo-by-van-anh-nguyen-from-pexels.jpg",
   },
   {
     name: "Mountain house",
-    link:
-      "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
+    link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/spots/6-photo-by-moritz-feldmann-from-pexels.jpg",
   },
 ];
 
@@ -56,15 +50,17 @@ const editModalDescriptionInput = editModal.querySelector(
 const cardTemplate = document.querySelector("#card-template");
 const cardList = document.querySelector(".card__list");
 
-function getCardElement(data){
-const cardElement = cardTemplate.content.querySelector(".card").cloneNode(true);
+function getCardElement(data) {
+  const cardElement = cardTemplate.content
+    .querySelector(".card")
+    .cloneNode(true);
 
-const cardNameEl = cardElement.querySelector(".card__title");
-const cardImageEl = cardElement.querySelector(".card__image");
-cardNameEl.textContent = data.name;
-cardImageEl.src = data.link;
-cardImageEl.alt = data.name;
-return cardElement;
+  const cardNameEl = cardElement.querySelector(".card__title");
+  const cardImageEl = cardElement.querySelector(".card__image");
+  cardNameEl.textContent = data.name;
+  cardImageEl.src = data.link;
+  cardImageEl.alt = data.name;
+  return cardElement;
 }
 
 function openedModal() {
@@ -88,3 +84,8 @@ profileEditButton.addEventListener("click", openedModal);
 editModalClosebtn.addEventListener("click", closedModal);
 
 editFormElement.addEventListener("submit", handleEditFormSubmit);
+
+function initialCards.forEach((item) => {
+  const cardEl = getCardElement(item);
+  cardList.append(cardEl);
+});
